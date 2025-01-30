9cbead547c6b67154e901266b0aabbf8ae6fbe14
Mathias Falvo <falvomathias@gmail.com>
2025-01-30 17:43:57 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            string input = args[0];
+            Console.WriteLine("Argument received: " + input);
+        }
+        else
+        {
+            Console.WriteLine("No arguments received.");
+        }
+    }
+}
